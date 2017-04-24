6ad6c28c61176ecf83eb3a66e4a99fd2bd100408
Yizhe Wang <wangyiz4262+git@gmail.com>
2017-04-24 00:15:24 -0400

    Migrated whole repo from Bitbucket to Github

---
+++ docs/js/script.js
@@ -0,0 +1,108 @@
+(function($){
+  // Nav bar toggle
+  $('#main-nav-toggle').on('click', function(){
+    $('.nav-container-inner').slideToggle();
+  });
+
+  // Share
+  $('body').on('click', function(){
+    $('.article-share-box.on').removeClass('on');
+  }).on('click', '.article-share-link', function(e){
+    e.stopPropagation();
+
+    var $this = $(this),
+      url = $this.attr('data-url'),
+      encodedUrl = encodeURIComponent(url),
+      id = 'article-share-box-' + $this.attr('data-id'),
+      offset = $this.offset();
+
+    if ($('#' + id).length){
+      var box = $('#' + id);
+
+      if (box.hasClass('on')){
+        box.removeClass('on');
+        return;
+      }
+    } else {
+      var html = [
+        '<div id="' + id + '" class="article-share-box">',
+          '<input class="article-share-input" value="' + url + '">',
+          '<div class="article-share-links">',
+            '<a href="https://twitter.com/intent/tweet?url=' + encodedUrl + '" class="article-share-twitter" target="_blank" title="Twitter"></a>',
+            '<a href="https://www.facebook.com/sharer.php?u=' + encodedUrl + '" class="article-share-facebook" target="_blank" title="Facebook"></a>',
+            '<a href="http://pinterest.com/pin/create/button/?url=' + encodedUrl + '" class="article-share-pinterest" target="_blank" title="Pinterest"></a>',
+            '<a href="https://plus.google.com/share?url=' + encodedUrl + '" class="article-share-google" target="_blank" title="Google+"></a>',
+          '</div>',
+        '</div>'
+      ].join('');
+
+      var box = $(html);
+
+      $('body').append(box);
+    }
+
+    $('.article-share-box.on').hide();
+
+    box.css({
+      top: offset.top + 25,
+      left: offset.left
+    }).addClass('on');
+  }).on('click', '.article-share-box', function(e){
+    e.stopPropagation();
+  }).on('click', '.article-share-box-input', function(){
+    $(this).select();
+  }).on('click', '.article-share-box-link', function(e){
+    e.preventDefault();
+    e.stopPropagation();
+
+    window.open(this.href, 'article-share-box-window-' + Date.now(), 'width=500,height=450');
+  });
+
+  // Caption
+  $('.article-entry').each(function(i){
+    $(this).find('img').each(function(){
+      if ($(this).parent().hasClass('fancybox')) return;
+
+      var alt = this.alt;
+
+      if (alt) $(this).after('<span class="caption">' + alt + '</span>');
+
+      $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>');
+    });
+
+    $(this).find('.fancybox').each(function(){
+      $(this).attr('rel', 'article' + i);
+    });
+  });
+
+  if ($.fancybox){
+    $('.fancybox').fancybox();
+  }
+
+  //Back to top
+  $("#back-to-top").on('click', function(){
+    $('body,html').animate({scrollTop:0}, 600);
+  });
+
+  // Baidu in-site search
+  // (function() {
+  //   document.write(unescape('%3Cdiv id="bdcs"%3E%3C/div%3E'));
+  //   var bdcs = document.createElement('script');
+  //   bdcs.type = 'text/javascript';
+  //   bdcs.async = true;
+  //   bdcs.src = 'http://znsv.baidu.com/customer_search/api/js?sid=10252577829139496308' + '&plate_url=' + encodeURIComponent(window.location.href) + '&t=' + Math.ceil(new Date() / 3600000);
+  //   var s = document.getElementsByTagName('script')[0];
+  //   s.parentNode.insertBefore(bdcs, s);
+  // })();
+
+  // Google custom search
+  // (function() {
+  //   var cx = '000144979510574537097:hull8jwat40';
+  //   var gcse = document.createElement('script');
+  //   gcse.type = 'text/javascript';
+  //   gcse.async = true;
+  //   gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+  //   var s = document.getElementsByTagName('script')[0];
+  //   s.parentNode.insertBefore(gcse, s);
+  // })();
+})(jQuery);
